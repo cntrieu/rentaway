56b0c50caa0a3d29a79ac831a01f2e2edc8e12d9
Calvin Trieu <calvin.trieu@hotmail.com>
2023-09-27 19:26:13 -0400

    render

--- server/index.js
+++ server/index.js
@@ -21,6 +21,12 @@ const app = express()
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://rentaway.onrender.com');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
+
 app.get("/", (req, res) => {
     res.setHeader("Access-Control-Allow-Credentials", "true");
     
